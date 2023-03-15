446298c9ee20f45ef34cf47f40fb32dd86167c21
Peter Perry <99445714+Lact0@users.noreply.github.com>
2023-03-15 23:40:52 +0000

    MCTS

--- ai.py
+++ ai.py
@@ -1,10 +1,12 @@
 from connect4 import *
+import math
 
 def value(board):
   state = checkWin(board)
   if state is not None:
     return checkWin(board) * 10
   return 0
+
 
 def minimax(board, depth, maxPlayer = False, a = -100, b = 100, memo = {}):
   
@@ -27,7 +29,6 @@ def minimax(board, depth, maxPlayer = False, a = -100, b = 100, memo = {}):
     sign = 1 if maxPlayer else -1
     newBoard = makeMove(board, sign, move)
     moveVal = .99 * minimax(newBoard, depth - 1, not maxPlayer, a, b, memo)[0]
-    
     if maxPlayer:
       if moveVal > best[0]:
         best = (moveVal, move)
@@ -44,3 +45,100 @@ def minimax(board, depth, maxPlayer = False, a = -100, b = 100, memo = {}):
   memo[key] = best
   
   return best
+
+
+
+
+class monteCarloNode():
+  def __init__(this, board, player):
+    this.board = board
+    this.player = player
+    this.wins = 0
+    this.visits = 0
+    this.children = {}
+    this.leaf = True
+    this.terminal = checkWin(this.board)
+    
+  def expand(this):
+    possibleMoves = np.unique(np.where(this.board == 0)[0])
+    moves = sorted(possibleMoves, key=lambda x: abs(3 - x))
+    sign = -1 + 2 * this.player
+    for move in moves:
+      newBoard = makeMove(this.board, sign, move)
+      child = monteCarloNode(newBoard, not this.player)
+      this.children[move] = child
+    this.leaf = False
+
+class monteCarloTree():
+  def __init__(this, rootBoard, rootPlayer):
+    this.root = monteCarloNode(rootBoard, rootPlayer)
+    this.c = 2 ** .5
+
+  def step(this):
+    nodeAddress = this.selectAndExpand()
+    this.simAndProp(nodeAddress)
+
+  def getBestMove(this):
+    bestMove = (None, -1, False)
+    possibleMoves = this.root.children
+    for move in possibleMoves:
+      child = this.root.children[move]
+      info = (move, child.wins / max(1, child.visits), True)
+      if info[1] > bestMove[1]:
+        bestMove = info
+    return bestMove
+
+  def makeMove(this, move):
+    if this.root.leaf:
+      this.root.expand()
+    if move not in this.root.children:
+      return False
+    this.root = this.root.children[move]
+    return True
+      
+  def selectAndExpand(this):
+    currentNode = this.root
+    address = []
+    while not currentNode.leaf:
+      bestMove = (None, -1)
+      for move in currentNode.children:
+        child = currentNode.children[move]
+        winRate = child.wins / max(1, child.visits)
+        innerTerm = math.log(currentNode.visits) / max(1, child.visits)
+        otherTerm = this.c * (innerTerm) ** .5
+        info = (move, winRate + otherTerm)
+        if info[1] > bestMove[1]:
+          bestMove = info
+      address.append(bestMove[0])
+      currentNode = currentNode.children[bestMove[0]]
+    if currentNode.terminal is None:
+      currentNode.expand()
+      firstMove = [x for x in currentNode.children][0]
+      address.append(firstMove)
+    return address
+
+  def simAndProp(this, address):
+    lastNode = this.root
+    for move in address:
+      lastNode = lastNode.children[move]
+      
+    board = np.copy(lastNode.board)
+    toGo = lastNode.player
+    while (winner := checkWin(board)) is None:
+      #possibleMoves = np.unique(np.where(board == 0)[0])
+      #move = np.random.choice(possibleMoves)
+      move = minimax(board, 1, toGo)[1]
+      sign = -1 + toGo * 2
+      board = makeMove(board, sign, move)
+      toGo = not toGo
+
+    currentNode = this.root
+    address.append(None)
+    for move in address:
+      currentNode.visits += 1
+      if winner == 0:
+        currentNode.wins += .5
+      elif currentNode.player == lastNode.player:
+        currentNode.wins += 1
+      if move is not None:
+        currentNode = currentNode.children[move]
--- main.py
+++ main.py
@@ -1,26 +1,89 @@
 from ai import *
 import time
 
-board = makeBoard()
-
 setupScreen()
-drawGrid(board)
 
-while True:
-  print('Make Move:')
-  move = int(input())
-  board = makeMove(board, 1, move)
+def playGame(isFirst = True):
+  board = makeBoard()
   drawGrid(board)
-  if checkWin(board) is not None:
-    print('You Won! (or Tie, I\'m too lazy to check.)')
-    break
-  oldTime = time.time()
-  move = minimax(board, 5)[1]
-  timeChange = int(time.time() - oldTime)
-  print('AI Chose', move, 'In', timeChange, 'seconds')
-  board = makeMove(board, -1, move)
-  drawGrid(board)
-  if checkWin(board) is not None:
-    print('You Lost! (or Tie, I\'m too lazy to check.)')
-    break
+  start = True
+  sign = -1 * (not isFirst) + 1 * isFirst
+  while True:
+    if isFirst or (not start):
+      print('Make Move:')
+      move = None
+      possibleMoves = np.unique(np.where(board == 0)[0])
+      while True:
+        move = input()
+        if move.isdigit() and int(move) < 7 and (int(move) in possibleMoves):
+          move = int(move)
+          break
+        print('INCORRECT, YOU IDIOT')
+      board = makeMove(board, sign, move)
+      drawGrid(board)
+      if checkWin(board) is not None:
+        print('You Won! (or Tie, I\'m too lazy to check.)')
+        break
+    else:
+      start = False
+        
+    oldTime = time.time()
     
+    move = minimax(board, 6, not isFirst)[1]
+    timeChange = int(time.time() - oldTime)
+    print('AI Chose', move, 'In', timeChange, 'seconds')
+    board = makeMove(board, -sign, move)
+    drawGrid(board)
+    if checkWin(board) is not None:
+      print('You Lost! (or Tie, I\'m too lazy to check.)')
+      break
+
+def playMonte(isFirst = True, moveTime = 30):
+  board = makeBoard()
+  tree = monteCarloTree(board, True)
+  drawGrid(board)
+  start = True
+  sign = -1 * (not isFirst) + 1 * isFirst
+  while True:
+    if isFirst or (not start):
+      print('Make Move:')
+      move = None
+      possibleMoves = np.unique(np.where(board == 0)[0])
+      while True:
+        move = input()
+        if move.isdigit() and int(move) < 7 and (int(move) in possibleMoves):
+          move = int(move)
+          break
+        print('INCORRECT, YOU IDIOT')
+      board = makeMove(board, sign, move)
+      tree.makeMove(move)
+      drawGrid(board)
+      if checkWin(board) is not None:
+        print('You Won! (or Tie, I\'m too lazy to check.)')
+        break
+    else:
+      start = False
+    oldTime = time.time()
+    i = 0
+    while time.time() - oldTime < moveTime:
+      tree.step()
+      i += 1
+    root = tree.root.children
+    #print([(x, root[x].wins / root[x].visits) for x in root])
+    move = tree.getBestMove()[0]
+    timeChange = int(time.time() - oldTime)
+    #print('AI Chose', move, 'In', timeChange, 'seconds')
+    print('After', i, 'iterations and', timeChange, 'seconds, the AI Chose', str(move) + '.')
+    percent = round(root[move].wins / root[move].visits * 100, 2)
+    print('It believes it has a ' + str(percent) + '% chance of winning.')
+    newBoard = makeMove(board, -sign, move)
+    if newBoard is False:
+      print('Tree:', tree.root.board, tree.root.children)
+      for move in tree.root.children:
+        print(move, tree.root.children[move].board)
+    board = newBoard
+    tree.makeMove(move)
+    drawGrid(board)
+    if checkWin(board) is not None:
+      print('You Lost! (or Tie, I\'m too lazy to check.)')
+      break
